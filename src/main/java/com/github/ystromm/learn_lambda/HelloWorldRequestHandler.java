8383dae9c661046e039ca358690346f97b905c1d
Martin Carlsson <martin.carlsson@omegapoint.se>
2017-07-27 22:51:37 +0200

    Renamed hello world handler.

--- src/main/java/com/github/ystromm/learn_lambda/HelloWorldRequestHandler.java
+++ src/main/java/com/github/ystromm/learn_lambda/HelloWorldRequestHandler.java
@@ -3,7 +3,7 @@ package com.github.ystromm.learn_lambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class LambdaRequestHandler implements RequestHandler<String, String> {
+public class HelloWorldRequestHandler implements RequestHandler<String, String> {
     public String handleRequest(String input, Context context) {
         context.getLogger().log("Input: " + input);
         return "Hello World - " + input;
